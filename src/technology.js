9c8649d05fcb98d3abbf80bf75a28440713017f9
Yana Talis <ziukhovaiana@gmail.com>
2024-07-14 20:45:15 +0300

    14.07

---
+++ src/technology.js
@@ -0,0 +1,14 @@
+const slider = document.querySelector('.swiper-container');
+
+let mySwiper = new Swiper(slider, {
+	slidesPerView: 1.5,
+	spaceBetween: 16,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	slideToClickedSlide: true,
+	centeredSlides: true,
+	
+})
